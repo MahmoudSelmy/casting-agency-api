5a6e926bae0741fe2f328f33b313b1005ac61401
mahmoudselmy06@gmail.com <mahmoud_sam_06>
2020-05-25 16:40:24 +0200

    + movies database access

---
+++ database/movie_access.py
@@ -0,0 +1,70 @@
+from models import Movie
+from utils import paginate_items
+
+
+class MovieAccess:
+    @classmethod
+    def get_all_movies(cls):
+        movies = Movie.query.all()
+        return movies
+
+    @classmethod
+    def get_movies_page(cls, page_number):
+        movies = cls.get_all_movies()
+        movies = paginate_items(page_number, movies)
+        movies = [movie.format() for movie in movies]
+        return movies
+
+    @classmethod
+    def get_total_number(cls):
+        movies = cls.get_all_movies()
+        return len(movies)
+
+    @classmethod
+    def _get_attribute_from_data(cls, data, attribute_name):
+        attribute = data.get(attribute_name, None)
+        if attribute is None:
+            raise ValueError(attribute_name + ' must be provided')
+        return attribute
+
+    @classmethod
+    def _convert_data_to_movie(cls, data):
+        title = cls._get_attribute_from_data(data, 'title')
+        release_date = cls._get_attribute_from_data(data, 'release_date')
+        movie = Movie(title, release_date)
+        return movie
+
+    @classmethod
+    def create_movie(cls, data):
+        try:
+            movie = cls._convert_data_to_movie(data)
+            movie.insert()
+        except Exception as e:
+            raise ValueError(str(e))
+        return movie
+
+    @classmethod
+    def get_movie_by_id(cls, movie_id):
+        drink = Movie.query.filter(Movie.id == movie_id).one_or_none()
+        if drink is None:
+            ValueError('Invalid Movie_id')
+        return drink
+
+    @classmethod
+    def update_movie(cls, movie_id, data):
+        movie = cls.get_movie_by_id(movie_id)
+
+        title = data.get('title', movie.title)
+        release_date = data.get('release_date', movie.release_date)
+
+        movie.title = title
+        movie.release_date = release_date
+
+        movie.update()
+
+        return movie
+
+    @classmethod
+    def delete_movie(cls, movie_id):
+        movie = cls.get_movie_by_id(movie_id)
+        movie.delete()
